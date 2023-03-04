b82acd4bfd97f71b98b35414c19562c06fb26fa0
meg63 <mechavarriac@unal.edu.co>
2023-03-04 13:14:51 -0500

    Primera ´prueba

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -10,15 +10,11 @@ public class Asignatura {
 
     public Asignatura() {
         this("Sin nombre",Tipo.FUNDAMENTACION);
-        this(0);
+       
     }
 
-    public Asignatura(int codigoInterno,Tipo tipo) {
-        this("Sin nombre", codigoInterno, 0,tipo);
-    }
-
-    public Asignatura(int codigoExterno,Tipo tipo) {
-        this("Sin nombre", 0, codigoExterno,tipo);
+    public Asignatura(int codigo,Tipo tipo) {
+        this("Sin nombre", codigo, codigo,tipo);
     }
 
     public Asignatura(String nombre, Tipo tipo) {
@@ -39,9 +35,7 @@ public class Asignatura {
         this.tipo = tipo;
     }
 
-    public void cambiarDatos(int codigoInterno) {
-        this.codigoInterno = codigoInterno;
-    }
+    
 
     public void cambiarDatos(int codigoExterno) {
         this.codigoExterno = codigoExterno;
